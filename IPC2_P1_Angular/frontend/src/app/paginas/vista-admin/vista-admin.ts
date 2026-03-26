23883891390cd45702ee9e893d3977edcdcb5ec0
julioOv-1104 <julioovalle202232237@cunoc.edu.gt>
2026-03-25 22:14:17 -0600

    primeros diseños del frontend

---
+++ IPC2_P1_Angular/frontend/src/app/paginas/vista-admin/vista-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-admin',
+  imports: [],
+  templateUrl: './vista-admin.html',
+  styleUrl: './vista-admin.css',
+})
+export class VistaAdmin {
+
+}
